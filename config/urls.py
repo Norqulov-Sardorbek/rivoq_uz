78e901cbaacba873059ea2579cce8ec2f91c1131
Sardorbek <nsardorbek776@gmail.com>
2026-01-06 20:43:20 +0500

    ok

--- config/urls.py
+++ config/urls.py
@@ -37,7 +37,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
-    path('courses/', include('courses.urls')),
+    # path('courses/', include('courses.urls')),
     # Third-party documentation URLs
     path('swaggercha<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swaggercha/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
--- courses/urls.py
+++ courses/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from courses.views.just_apii import Hug
 
 urlpatterns = [
-    path('hug/', Hug.as_view(), name='course_list'),
+    
 ]
